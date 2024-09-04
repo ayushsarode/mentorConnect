f3b944d5b979e682e1cd80447d63f6ed05b1088c
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-09-04 21:39:27 +0530

    directory setup
    Signed-off-by: Mayank Saharkar <mayanksaharkar@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,9 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import About from './routes/about';
-import Domains from './routes/domain';
-import Mentor from './routes/mentors';
-import Contacts from './routes/contact';
-import FAQ from './routes/FAQ';
-import Join from './routes/login';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './routes/home';
+import "./App.css";
+
+import MainRoutes from "./routes/MainRoutes";
 
 function App() {
-  return (
-    
-    <Router >
-      <Navbar />
-
-      <Routes>
-      <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/domains" element={<Domains />} />
-        <Route path="/mentor" element={<Mentor />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/join" element={<Join />} />
-      </Routes>
-    </Router>
-
-  );
+  return <MainRoutes />;
 }
 
 export default App;
