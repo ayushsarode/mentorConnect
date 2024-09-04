374f27f1415236671dcd7e6ad32004772aaaa869
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-09-04 21:46:11 +0530

    fix : HomePage
    Signed-off-by: Mayank Saharkar <mayanksaharkar@gmail.com>

---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer bg-base-200 text-base-content p-10 px-20">
+      <aside>
+        <p className="text-5xl font-bold tracking-tight text-foreground">
+          mentorConnect
+        </p>
+      </aside>
+
+      <nav>
+        <h6 className="footer-title">Links</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Mentors</a>
+        <a className="link link-hover">FaQ</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
