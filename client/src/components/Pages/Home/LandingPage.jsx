88f0a7af5ab04c274c695bb608e742c1716fab94
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-09-05 00:48:32 +0530

    hero section ui

--- client/src/components/Pages/Home/LandingPage.jsx
+++ client/src/components/Pages/Home/LandingPage.jsx
@@ -1,25 +1,37 @@
 import React from "react";
+import HeroGif from "../../../assets/hero.png"; 
 
 function LandingPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-[90dvh] bg-background px-4 py-12 sm:px-6 lg:px-8">
-      <div className=" text-center">
-        <h1 className="text-8xl font-bold tracking-tight text-foreground">
-          Do you have the best Mentor?
-        </h1>
-        <p className="mt-4 text-3xl text-muted-foreground">
-          Find or become a mentor to help you reach your full potential.
-        </p>
-        <div className="mt-6">
-          <a
-            href="#"
-            className="inline-flex items-center rounded-md bg-primary text-base-100 px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
-          >
-            Find a Mentor
-          </a>
-        </div>
-      </div>
+<div className="flex flex-col lg:flex-row items-center justify- h-[92vh] bg-background px-4 gap-[8rem] py-10 sm:px-6 lg:px-8">
+  <div className="lg:w-1/2 text-center lg:text-left">
+    <h1 className="text-5xl lg:text-8xl font-bold tracking-tight text-foreground">
+    <span className="font-normal">Do you have the best</span>  Mentor?
+    </h1>
+    <p className="mt-4 text-xl lg:text-3xl text-muted-foreground">
+     Find or become a mentor to help you reach your full potential.
+    </p>
+    <div className="mt-6">
+      <a
+        href="#"
+        className="inline-flex items-center rounded-md bg-primary text-base-100 px-7 py-4 text-lg font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+      >
+        Find a Mentor
+      </a>
     </div>
+  </div>
+  
+  <div className="lg:w-1/2 mt-10 lg:mt-0 flex justify-center">
+    <div className="carousel w-full space-x-4">
+      <div className="carousel-item">
+        <img src={HeroGif} alt="Tech Person 1" className="rounded-full w-[40rem] object-cover" />
+      </div>
+
+  
+    </div>
+  </div>
+</div>
+
   );
 }
 
--- client/src/components/Pages/Mentors/Dynamicmentors.jsx
+++ client/src/components/Pages/Mentors/Dynamicmentors.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Dynamicmentors = ({ mentorId }) => {
   // Find the mentor by ID
-  const mentor = mentor.find((m) => m.id === mentorId);
+  const mentor = mentorsData.find((m) => m.id === mentorId);
 
   if (!mentor) {
     return <div>Mentor not found</div>;
